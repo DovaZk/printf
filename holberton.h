0d49d79add573ef17e64c89d24849c41fc10b7b7
giosermon <2172@holbertonschool.com>
2021-03-13 16:54:47 -0500

    lib.h

---
+++ holberton.h
@@ -0,0 +1,31 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * struct s_type - Structure
+ * @args: pointer arguments
+ * @func: pointer function
+ */
+typedef struct f_type
+{
+  char *args;
+  int (*func)(va_list);
+} f_type;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int (*_typefor(const char *argu, int argb))(va_list);
+int print_char(va_list valist);
+int print_string(va_list valist);
+int print_int(va_list valist);
+int count_num(unsigned int n);
+int print_rot13(char *s);
+int print_R(va_list valist);
+int print_binary(va_list valist);
+
+#endif
