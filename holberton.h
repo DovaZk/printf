41d7a1d5606f1cb923778c0a03ad7c2aafa940b8
giosermon <2172@holbertonschool.com>
2021-03-14 14:21:53 -0500

    lib

--- holberton.h
+++ holberton.h
@@ -6,16 +6,6 @@
 #include <string.h>
 #include <unistd.h>
 
-int _putchar(char c);
-int _printf(const char *format, ...);
-int (*_typefor(const char *argu, int argb))(va_list);
-int print_char(va_list valist);
-int print_string(va_list valist);
-int print_int(va_list valist);
-int count_num(unsigned int n);
-int print_rot13(char *s);
-int print_R(va_list valist);
-int print_binary(va_list valist);
 
 #endif
 
