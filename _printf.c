ccd346aebdfa0190788adb9450542e6d850fea4d
juanstm200 <juans.tovar70@gmail.com>
2021-03-16 18:59:12 +0100

    main_function

---
+++ _printf.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * _printf - print string or character fromo main
+ * @format: enter string or other char
+ * Return: the lentgh of the string
+ */
+int _printf(const char *format, ...)
+{
+	char buffer[1024];
+
+	int (*form)(va_list);
+
+	strcopy(buffer, format);
+
+	int lent = strle(buffer);
+	int i = 0;
+	char c;
+
+	va_list lista;
+
+	va_start(lista, format);
+
+	for (; buffer[i]; i++)
+	{
+		if (buffer[i] == '%')
+		{
+			i++;
+			form = op_menu((buffer + i), lista);
+			form(lista);
+		}
+		else
+		{
+			c = buffer[i];
+			_putchar(c);
+		}
+	}
+
+	va_end(lista);
+	return (lent);
+}
