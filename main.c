933cd0e374a249fadd453b9f331dced75a058ca7
juanstm200 <juans.tovar70@gmail.com>
2021-03-16 19:13:09 +0100

    main2

--- main.c
+++ main.c
@@ -5,6 +5,6 @@ int main(void)
 	char p = 'K';
 	char *p1 = "joel";
 	char *p2 = "sergio";
-	int lent = _printf("Hola compañeros [%c], [%s], [%s]", p, p1, p2);
+	int lent = _printf("Hola compañeros [%c], [%s], [%s]\n", p, p1, p2);
 	printf("\n%d\n", lent);
 }
