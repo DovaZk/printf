35ffa869399791a44ed678795d7e8d40f5c29e10
giosermon <2172@holbertonschool.com>
2021-03-16 12:37:04 -0500

    bookshop

--- holberton.h
+++ holberton.h
@@ -1,11 +1,36 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <stdio.h>
-#include <stdarg.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <stdarg.h>
 
+int _printf(const char *format, ...);
+
+int strle(char *str);
+
+void strcopy(char *btr, const char *str);
+
+/*Writes a character*/
+int _putchar(char c);
+
+void *op_menu(char *s, ...);
+
+void pchar(va_list charl);
+
+/*Writes a string*/
+void pstring(va_list charl);
+
+/**
+ * struct datos - Structure
+ * @s: pointer arguments
+ * @func: pointer function
+ */
+
+typedef struct datos
+{
+	char *s;
+	void (*func)(va_list);
+} dato;
 
 #endif
-
