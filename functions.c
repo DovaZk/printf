b766f6d6e3d094e7f8959cb57e7cada1a49fc43d
giosermon <2172@holbertonschool.com>
2021-03-16 12:41:08 -0500

    Functions using the [s] and [c] specifiers

---
+++ functions.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * pchar - print char.
+ * @charl: argument
+ * Return: Void
+ */
+void pchar(va_list charl)
+{
+	char p = va_arg(charl, int);
+		_putchar(p);
+}
+/**
+ * pstring - print string.
+ * @charl: argument
+ * Return: Void
+ */
+void pstring(va_list charl)
+{
+	char *str = va_arg(charl, char *);
+	int lent = strle(str);
+	int i = 0;
+
+	for (; str[i]; i++)
+	{
+	if (str != NULL)
+	{
+	_putchar(str[i]);
+	}
+	}
+}
